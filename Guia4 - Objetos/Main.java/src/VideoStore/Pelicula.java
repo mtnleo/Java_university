b83557389a3f270e08c6352abfda9e25124b8ecc
mtnleo <mtnleonardi@gmail.com>
2023-04-03 11:50:55 -0300

    Guia 4 empezada, defininiendo las clases

---
+++ Guia4 - Objetos/Main.java/src/VideoStore/Pelicula.java
@@ -0,0 +1,39 @@
+package VideoStore;
+
+import java.time.LocalDate;
+
+public class Pelicula {
+    protected String nombre;
+    protected String genero;
+    protected LocalDate fecha_lanzamiento;
+    protected int duracion;
+    protected String clasificacion;
+    protected String pais;
+    protected String descripcion;
+
+    protected int copias;
+
+    // GETTERS & SETTERS ---------------------------------------------------
+
+    public int getCopias() {
+        return copias;
+    }
+
+    public void setCopias(int copias) {
+        this.copias = copias;
+    }
+
+    // BUILDER ---------------------------------------------------
+
+    public Pelicula(String nombre, String genero, LocalDate fecha_lanzamiento, int duracion, String clasificacion, String pais, String descripcion) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.fecha_lanzamiento = fecha_lanzamiento;
+        this.duracion = duracion;
+        this.clasificacion = clasificacion;
+        this.pais = pais;
+        this.descripcion = descripcion;
+
+    }
+
+}
